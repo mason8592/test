cfce95a1db0f14c59bb01bff912560d457dfe4ce
mason8592 <mrjellyman223@gmail.com>
2022-06-15 06:39:24 -0500

    add back old files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
+const path = require('path');
+
+app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
-  res.send("Hello World!")
+  res.sendFile(path.join(__dirname+'/index.html'))
 })
 
 app.listen(3000, function () {
