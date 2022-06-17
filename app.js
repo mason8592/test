51722bea44069d3233eafe6ec1526ba643ffaaa8
mason8592 <mrjellyman223@gmail.com>
2022-06-17 09:30:43 -0500

    Create better page system, fix up mom and rename it to reverse

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname+"/pages")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'))
+  res.sendFile(path.join(__dirname+'/pages/home/index.html'))
 })
 
 app.listen(3000, function () {
