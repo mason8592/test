411eea663e145126c2487222350974552b523a32
mason8592 <mrjellyman223@gmail.com>
2022-06-22 16:32:22 -0500

    Rewrite site

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express")
 const app = express()
 const path = require('path');
 
-app.use(express.static(__dirname + "/pages/"));
+app.use(express.static(path.join(__dirname + "/public/pages")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname+'/pages/home/index.html'))
+  res.sendFile(path.join(__dirname+'/public/pages/home/index.html'))
 })
 
 app.listen(process.env.PORT || 3000, function () {
