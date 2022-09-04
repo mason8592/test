1e54c591644b359fba08c3c2937126856e700834
mason8592 <mrjellyman223@gmail.com>
2022-09-04 15:43:42 -0500

    Fix CSP

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const favicon = require("serve-favicon")
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://*; script-src 'self' 'unsafe-inline';");
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Content-Security-Policy": "default-src 'self' 'unsafe-inline'; img-src 'self' https://*; script-src 'self' 'unsafe-inline';",
+        "X-Content-Security-Policy": "default-src *",
+        "X-WebKit-CSP": "default-src *"
+    })
+
     return next();
 });
 
