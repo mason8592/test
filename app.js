dcd4ff2e150fea175558c38d4743272f98fd05e5
mason8592 <mrjellyman223@gmail.com>
2022-09-09 10:50:34 -0500

    Add support for future POST requests

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require("express")
+const bodyParser = require("body-parser");
+const router = express.Router();
 const app = express()
 const path = require('path');
 const favicon = require("serve-favicon")
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// router.post("/handle", async (request, response) => {
+
+// })
 
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 app.use(function(req, res, next) {
     res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://*; script-src 'self' 'unsafe-inline';");
-    return next();
-});
+    return next()
+})
 
 app.use(express.static(path.join(__dirname + "/public/pages")));
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/home/index.html'))
+    res.sendFile(path.join(__dirname+'/public/home/index.html'))
 })
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Up and running")
+    console.log("Up and running")
 })
+
+app.use("/", router)
